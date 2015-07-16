c5a7cb9700f5755afa471e1afadd1606d3059502
Earl Bellinger <earlbellinger@gmail.com>
2015-07-16 20:40:46 +0200

    Update HR.R

--- HR.R
+++ HR.R
@@ -6,10 +6,12 @@
 library(Hmisc)
 
 #simulation_dir <- 'exp_alpha'
-simulation_dir <- 'exp_Y'
+#simulation_dir <- 'exp_Y'
+for (simulation_dir in c('exp_alpha', 'exp_Y')) {
 
+dirs <- dir(simulation_dir)
 plot_dir <- paste0('plots_', simulation_dir)
-dir.create(plot_dir)
+dir.create(plot_dir, showWarnings=FALSE)
 
 if (simulation_dir == 'exp_Y') {
     exp <- 'helium content'
@@ -26,14 +28,17 @@ solar_age = 4.57e9
 
 mar <- c(4, 4.5, 3, 1)
 mgp <- c(3, 0.25, 0)
-approx_xout <- seq(0, 1, .01)
+approx_xout <- seq(0, 1, .04)
 font <- "Palatino"
 
-### Plot HR diagram
 freep <- as.name(sub('.+_', '', simulation_dir))
-dirs <- dir(simulation_dir)
-offset <- 4
-cl <- rev(heat.colors(length(dirs)+offset))[offset:(offset+length(dirs)-1)]
+offset <- 6
+cl <- heat.colors(length(dirs)+offset)[1:length(dirs)]
+labels <- sapply(sub('.+_', '', dirs), 
+    function(x) { as.expression(bquote(.(freep) ~ "=" ~ .(x))) })
+
+### Plot HR diagram
+print("Plotting HR diagram")
 cairo_pdf(file.path(plot_dir, 'HR.pdf'), width=6, height=6, family=font)
 sim_i <- 0
 model_nos <- c()
@@ -49,7 +54,7 @@ for (simulation in dirs) {
                  xlab=expression(log~T[eff]),
                  ylab=expression(log~L/L["⊙"]), 
                  main=paste("HR diagram of Sun-like stars with varied", exp),
-                 xlim=rev(c(min(data$log_Teff), 
+                 xlim=rev(c(min(data$log_Teff)-0.01, 
                             max(data$log_Teff)+0.05)),
                  ylim=c(floor(min(data$log_L)),
                         ceiling(max(data$log_L))),
@@ -58,46 +63,41 @@ for (simulation in dirs) {
         } else {
             lines(HR, col=cl[sim_i])
         }
-        #sun_like <- Teff_sun < data$log_Teff+.005 & 0 < data$log_L+.05 & 
-        #            Teff_sun > data$log_Teff-.005 & 0 > data$log_L-.05
-        #model_nos <- c(model_nos, data$model_number[sun_like])
     }
 }
 points(Teff_sun, 0, col="black")
 points(Teff_sun, 0, col="black", pch=21, cex=0.1)
-labels <- sapply(sub('.+_', '', dirs), 
-                 function(x) { as.expression(bquote(.(freep) ~ "=" ~ .(x))) })
 legend("topleft", col=cl, lty=1, bty='n', legend=labels)
 dev.off()
 
 bcl <- c(cl[1:(sun_num-1)], "black", cl[(sun_num+1):length(cl)])
 
-### Plot internal profiles
+### Find profile files at the solar age
+print("Finding profile files at the solar age")
 modelS <- read.table('modelS.dat', header=TRUE) 
-#Mode <- function(x) {
-#  ux <- unique(x)
-#  ux[which.max(tabulate(match(x, ux)))]
-#}
-#model_no <- Mode(model_nos) # find nearest solar-like model
 profile_nos <- c()
 for (simulation in dirs) {
     log_dir <- file.path(simulation_dir, simulation, 'LOGS')
     log_files <- dir(log_dir)
+    log_files <- log_files[order(as.numeric(gsub('\\D', '', log_files)))]
     best_log <- NA
     best_age <- Inf
-    for (log_i in grep('profile\\d+.data', log_files)) {
+    for (log_i in grep('profile\\d+.data$', log_files)) {
         log_file <- file.path(log_dir, log_files[log_i])
         age <- read.table(log_file, skip=1, nrows=1, header=TRUE)$star_age
         age_diff <- abs(age - solar_age)
+        print(c(log_file, best_age, age_diff))
         if (age_diff < best_age) {
             best_log <- log_file
             best_age <- age_diff
-            if (age_diff > best_age) break
         }
+        if (age_diff > best_age) break
     }
     profile_nos <- c(profile_nos, best_log)
 }
 
+### Plot internal profiles
+print("Plotting internal profiles")
 for (col_name in c("csound", "logRho", "pressure", "temperature")) {
     if (col_name=="csound") {
         modelS_y <- log10(modelS[['csound']])
@@ -126,12 +126,6 @@ for (col_name in c("csound", "logRho", "pressure", "temperature")) {
     y_max <- max(modelS_y)
     x_max <- 1
     for (simulation in dirs) {
-        #profile_index <- file.path(simulation_dir, simulation, 
-        #                           'LOGS', 'profiles.index')
-        #profiles <- read.table(profile_index, skip=1)
-        #profile_no <- profiles[which(profiles[,1]==model_no), 3]
-        #data_file <- file.path(simulation_dir, simulation, 'LOGS', 
-        #                       paste0('profile', profile_no, '.data'))
         data_file <- file.path(profile_nos[grep(simulation, profile_nos)])
         if (file.exists(data_file)) {
             data <- read.table(data_file, header=TRUE, skip=5)
@@ -158,12 +152,6 @@ for (col_name in c("csound", "logRho", "pressure", "temperature")) {
               width=6, height=6, family="Palatino")
     sim_i <- 0
     for (simulation in dirs) {
-        #profile_index <- file.path(simulation_dir, simulation, 
-        #                           'LOGS', 'profiles.index')
-        #profiles <- read.table(profile_index, skip=1)
-        #profile_no <- profiles[which(profiles[,1]==model_no),3]
-        #data_file <- file.path(simulation_dir, simulation, 'LOGS', 
-        #                       paste0('profile', profile_no, '.data'))
         data_file <- file.path(profile_nos[grep(simulation, profile_nos)])
         if (file.exists(data_file)) {
             sim_i <- sim_i + 1
@@ -199,36 +187,46 @@ for (col_name in c("csound", "logRho", "pressure", "temperature")) {
     dev.off()
     
     ## plot relative differences
-    cairo_pdf(file.path(plot_dir, paste0('profile_diff_', col_name, '.pdf')), 
+    cairo_pdf(file.path(plot_dir, paste0('diff_', col_name, '.pdf')), 
               width=6, height=6, family=font)
     S_interp <- approx(modelS$radius, modelS_y, n=length(approx_xout), 
                        xout=approx_xout, rule=2)$y
     rel_diff <- apply(interps, 2, 
-                      function(x) {abs((x-S_interp)/max(x, S_interp))})
-    core_max <- max(rel_diff[1:20,])
-    r_max <- min(which(rel_diff[20:nrow(rel_diff),]>core_max, 
-                 arr.ind=TRUE)[,1])+20
+    #                  function(x) {abs((x-S_interp)/max(x, S_interp))})
+                      function(x) {(x-S_interp)/S_interp})
+    #core_max <- max(rel_diff[1,])
+    #r_max <- min(which(rel_diff[2:nrow(rel_diff),]>core_max, 
+    #                   arr.ind=TRUE)[,1])+1
+    core_var <- var(rel_diff[1,])
+    offset <- round(nrow(rel_diff) * 0.45, 0)
+    r_max <- min(which(apply(rel_diff[offset:nrow(rel_diff),], 1, 
+                             var)>core_var))+offset
     x_max <- max(approx_xout[1:r_max])
-    y_max <- max(rel_diff[1:r_max])+.01*max(rel_diff[1:r_max])
-    for (sim_i in 1:ncol(interps)) {
+    for (sim_i in 1:ncol(rel_diff)) {
         if (sim_i == 1) {
-            par(bty="l", las=1, mar=mar, mgp=mgp)
-            plot(approx_xout[1:r_max], rel_diff[1:r_max,sim_i], 
+            par(bty='l', las=1, mar=mar+c(0,0,0,6), mgp=mgp, xpd=TRUE)
+            plot(approx_xout[1:r_max], rel_diff[1:r_max, sim_i], 
                  type='l', col=cl[sim_i],
-                 xaxs='i', yaxs='i', tck=0.01,
+                 xaxs='i', yaxs='i', 
+                 tck=0.01,
                  xlim=c(0, x_max),
-                 ylim=c(0, y_max),
-                 ylab=bquote(abs((.(symbol)[.(freep)] - .(symbol)[S])
-                     /max*"("*.(symbol)[S]*","~.(symbol)[.(freep)]*")")),
+                 ylim=range(rel_diff[1:r_max,]),
+                 #ylab=bquote(abs((.(symbol)[.(freep)] - .(symbol)[S])
+                 #    /max*"("*.(symbol)[S]*","~.(symbol)[.(freep)]*")")),
+                 ylab=bquote((log~.(symbol)[.(freep)] - log~.(symbol)[S]) 
+                            / log~.(symbol)[S]),
                  xlab=expression(r/R["⊙"]),
                  main=paste("Relative", tolower(main_label),
                             "difference with\nModel S at the solar age"))
             minor.tick(nx=5, ny=5, tick.ratio=-0.25)
+            lines(c(x_max,0), c(0, 0), lty=2)
         } else {
             lines(approx_xout[1:r_max], rel_diff[1:r_max, sim_i], 
                   col=bcl[sim_i])
         }
     }
-    legend(per_off*x_max, y_max, legend=labels, col=bcl, lty=1, bty='n')
+    legend("right", legend=labels, col=bcl, lty=1, bty='n', inset=c(-0.35,0))
     dev.off()
+}
+
 }
