17edbac34142e087f14ca53f615fb28916295912
Earl Bellinger <earlbellinger@gmail.com>
2015-07-29 20:38:43 +0200

    Update fgong.R

--- fgong.R
+++ fgong.R
@@ -6,7 +6,7 @@
 source('utils.R')
 
 fgong_dir <- file.path('fgongs')
-plot_dir <- file.path('freq_plots')
+plot_dir <- file.path('plots')
 dir.create(plot_dir, showWarnings=FALSE)
 
 modelS <- read.table('fgong.l5bi.d.dat')
@@ -25,16 +25,17 @@ for (exp_dir in list.dirs(fgong_dir, recursive=FALSE)) {
         load_experiment_info(exp_dir, dirs)
         
         widths <- c(.44, .44, .12)
-        start_dev("Frequency differences in", "diffs", 
+        start_dev("Frequency differences in", "freqdiffs", 
                   basename(exp_dir), ev_stage, width=widths)
         start_dev("Frequencies of", "freqs", basename(exp_dir), ev_stage,
                   widths)
-            
+        y_min <- Inf
         y_max <- -Inf
         x_max <- -Inf
         x_min <- Inf
         for (model_i in data_files) {
             data <- read.table(file.path(ev_stage_dir, files[model_i]))
+            if (min(data[,3]) < y_min) y_min <- min(data[,3])
             if (max(data[,3]) > y_max) y_max <- max(data[,3])
             if (max(data[,2]) > x_max) x_max <- max(data[,2])
             if (min(data[,2]) < x_min) x_min <- min(data[,2])
@@ -66,26 +67,29 @@ for (exp_dir in list.dirs(fgong_dir, recursive=FALSE)) {
                 if (counter == 1) {
                     par(bty="l", las=1, mar=c(3, 3.6, 1, 1), 
                         mgp=c(2.2, 0.25, 0))
-                    plot(relation, pch=20, cex=0.01,
+                    plot(relation, pch=20, cex=0.01, lwd=0.5,
                          xlim=c(min(0, x_min-1), x_max), 
                          ylim=c(0, round(y_max+50, -2)),
                          yaxs='i', xaxs='i',
                          col=tcl[counter], 
                          tck=0.01, 
                          xlab='',
-                         ylab=bquote(.(l_name)~frequency~nu~"["*mu*Hz*"]"))
+                         ylab=bquote(.(l_name)~"frequency"~nu~"["*mu*Hz*"]"))
+                                     #~omega~"["*rad/s*"]"))
+                                     #"["*mu*Hz*"]"))
                     title(xlab=expression("radial order"~n), 
                           mgp=par()$mgp-c(0.4,0,0))
-                    minor.tick(nx=5, ny=5, tick.ratio=-0.25)
+                    minor.tick(nx=5, ny=5, tick.ratio=-0.15)
                 } else {
-                    points(relation, pch=20, cex=0.01, col=tcl[counter])
+                    points(relation, pch=20, cex=0.01, 
+                           col=tcl[counter], lwd=0.5)
                 }
                 counter <- counter + 1
             }
             if (grepl('solar-like', ev_stage)) {
                 ell <- modelS[modelS[,1]==l_mode,]
                 relation <- ell[,3] ~ ell[,2]
-                points(relation, pch='o', cex=0.5, col="black")
+                points(relation, pch=1, cex=0.5, col="black", lwd=0.5)
                 ref <- ell
             }
             if (l_mode==1) make_legend(labls, ev_stage, lty=FALSE)
@@ -103,15 +107,13 @@ for (exp_dir in list.dirs(fgong_dir, recursive=FALSE)) {
                 ell <- data[data[,1]==l_mode,]
                 x_counter <- 1
                 for (x in n_range) {#x_min:x_max) {
-                    #print(c(x_counter, counter, max(ref[,2])))
                     diffs[x_counter, counter] <- 
                         if (x%in%ref[,2] && x%in%ell[,2]) {
-                            as.numeric((ell[ell[,2]==x,][1,3] -
-                                        ref[ref[,2]==x,][1,3]) / 
-                                        ref[ref[,2]==x,][1,3])
+                            a <- 2*pi*Mode(unlist(ell[ell[,2]==x,][3]))
+                            b <- 2*pi*Mode(unlist(ref[ref[,2]==x,][3]))
+                            (a-b)/b
                             } else NA
                     x_counter <- x_counter + 1
-                    #print(diffs)
                 }
                 counter <- counter + 1
             }
@@ -124,18 +126,18 @@ for (exp_dir in list.dirs(fgong_dir, recursive=FALSE)) {
                          diffs[,counter], pch=20, cex=0.01,
                          xlim=c(min(0, min(n_range)-1), max(n_range)), 
                          ylim=range(diffs[complete.cases(diffs)]),
-                         xaxs='i',
+                         xaxs='i', lwd=0.5,
                          col=tcl[counter], 
                          tck=0.01, 
                          xlab='',
-                         ylab=bquote({delta * nu["\u2113"==.(l_mode)] /
-                                      nu["\u2113"==.(l_mode)]}))
+                         ylab=bquote({delta * omega["\u2113"==.(l_mode)] /
+                                      omega["\u2113"==.(l_mode)]}))
                     title(xlab=expression("radial order"~n), 
                           mgp=par()$mgp-c(0.4,0,0))
-                    minor.tick(nx=5, ny=5, tick.ratio=-0.25)
+                    minor.tick(nx=5, ny=5, tick.ratio=-0.15)
                 } else {
                     points(n_range,#x_min:x_max, 
-                           diffs[,counter], pch=20, cex=0.01, 
+                           diffs[,counter], pch=20, cex=0.01, lwd=0.5,
                            col=tcl[counter])
                 }
                 counter <- counter + 1
@@ -143,7 +145,7 @@ for (exp_dir in list.dirs(fgong_dir, recursive=FALSE)) {
             if (grepl('solar-like', ev_stage)) {
                 ell <- modelS[modelS[,1]==l_mode,]
                 relation <- rep(0, length(ell[,2])) ~ ell[,2]
-                points(relation, pch='o', cex=0.5, col="black")
+                points(relation, pch=1, cex=0.5, col="black", lwd=0.5)
             }
             if (l_mode==1) make_legend(labls, ev_stage, lty=FALSE)
             dev.set(dev.next())
