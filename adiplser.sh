ffe5b13d34c5cf75b1360176b446354117ea161a
Earl Bellinger <earlbellinger@gmail.com>
2015-08-24 18:32:52 +0200

    Update adiplser.sh

--- adiplser.sh
+++ adiplser.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 for exp in fgongs/*; do
-    for ev_stage in "$exp"/RGB_bump; do
+    #for ev_stage in $exp/RGB; do
+    for ev_stage in $exp/*; do
         for fgong in "$ev_stage"/*.FGONG; do
-            ./fgong2freqs.sh $fgong &
-            exit
+            if grep -q 'RGB' <<< $fgong; then continue; fi
+            if [[ -f $fgong ]]; then
+                ./fgong2freqs.sh $fgong &
+                #echo $fgong
+            fi
         done
     done
 done
