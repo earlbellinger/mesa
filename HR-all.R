4430ca394b4ec7a785e34ad990efb94cda374cbf
Earl Bellinger <earlbellinger@gmail.com>
2015-11-09 13:05:09 +0100

    Update HR-all.R

--- HR-all.R
+++ HR-all.R
@@ -1,153 +1,166 @@
 library(magicaxis)
 library(RColorBrewer)
 library(akima)
+library(parallel)
+library(parallelMap)
 
 font <- 'Palatino'
+
+plot_dir <- file.path('plots')
+dir.create(plot_dir, showWarnings=FALSE)
 
 sim_dir <- 'deleter'
 simulations <- file.path(sim_dir, list.files(sim_dir))
 simulations <- simulations[grep('.dat', simulations)]
-sims.DF <- do.call(rbind, Map(function (f) read.table(f, header=1), simulations))
-sims.DF <- sims.DF[sims.DF$dnu02_median > 0 & sims.DF$dnu02_median < 15,]
-write.table(sims.DF, file.path('grids', 'deleter.dat'), quote=FALSE, sep='\t', row.names=FALSE)
-combos <- unique(sims.DF[,1:4])
 
-# L_lim <- c(0)
-# T_lim <- c()
-# Dnu_lim <- c()
-# dnu_lim <- c()
-# Dnus <- c()
-# dnus <- c()
-# r02 <- c()
-# r01 <- c()
-# r13 <- c()
-# ages <- c()
-# max_R <- 0
-# max_M <- 0
-# for (simulation in simulations) {
-    # DF <- read.table(simulation, header=1)
-    # DF <- DF[DF$dnu02_median > 0 & DF$dnu02_median < 15,]
-    # L_lim <- range(L_lim, DF$L)
-    # T_lim <- range(T_lim, DF$Teff)
-    # Dnu_lim <- range(Dnu_lim, DF$Dnu_median)
-    # dnu_lim <- range(dnu_lim, DF$dnu02_median)
-    # Dnus <- c(Dnus, DF$Dnu_median)
-    # dnus <- c(dnus, DF$dnu02_median)
-    # r02 <- c(r02, DF$r_sep02_median)
-    # r01 <- c(r01, DF$r_avg01_median)
-    # r13 <- c(r13, DF$r_sep13_median)
-    # ages <- c(ages, DF$age)
-    # if (max(DF$radius) > max_R) max_R <- max(DF$radius)
-    # if (max(DF$mass) > max_M) max_M <- max(DF$mass)
-# }
+parallelStartMulticore(max(1, detectCores()))
+seis.DF <- do.call(rbind, 
+    parallelMap(function (f) read.table(f, header=1), simulations))
 
-cairo_pdf('HR.pdf', width=4, height=3, family=font)
+print("Before outlier removal:")
+print(nrow(seis.DF))
+print(sapply(seis.DF, fivenum))
+
+get_outliers <- function(DF) {
+    unique(unlist(parallelMap(
+        function(ii) which(DF[,ii] %in% boxplot.stats(DF[,ii], coef=10)$out),
+        1:ncol(DF))))
+}
+outliers <- get_outliers(seis.DF)
+while (length(outliers) > 0) {
+    seis.DF <- seis.DF[!(1:nrow(seis.DF)) %in% outliers,]
+    #combos <- unique(seis.DF[,1:4])
+    #tracks <- parallelMap(function(i) 
+    #    nrow(merge(combos[i,], seis.DF)), 1:nrow(combos))
+    
+    singletons <- c()
+    for (ii in which(!duplicated(seis.DF[,1:4])))
+        if (!(ii+1 < nrow(seis.DF) && all(seis.DF[ii,1:4] == seis.DF[ii+1,1:4])
+            | ii-1 > 0 && all(seis.DF[ii,1:4] == seis.DF[ii-1,1:4])))
+                singletons <- c(singletons, ii)
+    if (!is.null(singletons)) {
+        print(paste("Removing", length(singletons), "singletons"))
+        seis.DF <- seis.DF[-singletons,]
+    }
+    
+    #seis.DF <- seis.DF[parallelMap(function(i) 
+    #    length(merge(seis.DF[i,1:5], seis.DF))>1, 1:nrow(seis.DF)),]
+    #seis.DF <- seis.DF[unlist(parallelMap(function(i) 
+    #    i+1 < nrow(seis.DF) && all(seis.DF[i,1:4] == seis.DF[i+1,1:4]) | 
+    #    i-1 > 0 && all(seis.DF[i,1:4] == seis.DF[i-1,1:4]), 1:nrow(seis.DF))),]
+    #seis.DF <- seis.DF[duplicated(seis.DF[,1:4]),]
+    outliers <- get_outliers(seis.DF)
+}
+
+print("After outlier removal:")
+print(nrow(seis.DF))
+print(sapply(seis.DF, fivenum))
+
+write.table(seis.DF, file.path('grids', 'deleter.dat'), quote=FALSE, 
+    sep='\t', row.names=FALSE)
+combos <- unique(seis.DF[,1:4])
+#ages <- sapply(1:nrow(combos), function(i) max(merge(combos[i,], seis.DF)$age))
+ages <- parallelMap(function(i) max(merge(combos[i,], seis.DF)$age), 
+    1:nrow(combos))
+combos <- combos[order(ages),]
+
+
+png(file.path(plot_dir, 'HR.png'), width=600, height=450, family=font)
 par(mar=c(3, 4, 1, 1), mgp=c(2, 0.25, 0), cex.lab=1.3)
-#for (simulation_i in 1:length(simulations)) {
-    #DF <- read.table(simulations[simulation_i], header=1)
-    #DF <- DF[DF$dnu02_median > 0 & DF$dnu02_median < 15,]
 for (simulation_i in 1:nrow(combos)) {
-    DF <- merge(combos[simulation_i,], sims.DF)
-    HR <- DF$L ~ DF$Teff
+    DF <- merge(combos[simulation_i,], seis.DF)
+    HR <- DF$L ~ log10(DF$Teff)
+    hrcex <- 0.1*DF$M/max(seis.DF$M)
+    hrcol <- brewer.pal(10,"Spectral")[floor(DF$age/13.9*10)+1]
     if (simulation_i == 1) {
-        plot(HR, type='l', tcl=0,
-            #ylim=L_lim, xlim=rev(T_lim),
-            ylim=range(sims.DF$L, 0), xlim=rev(range(sims.DF$Teff)),
-            xlab=expression(T[eff]), xaxt='n', yaxt='n',
-            ylab=expression(L / L['\u0298']))
-        abline(v=5777, lty=3, col='lightgray')
-        abline(h=1, lty=3, col='lightgray')
+        plot(HR, #type='l', 
+            tcl=0, pch=20, cex=hrcex, col=hrcol, #lwd=0.1,
+            ylim=range(seis.DF$L, 0), xlim=rev(log10(range(seis.DF$Teff))),
+            xlab=expression(log[10]~T[eff]), xaxt='n', yaxt='n',
+            ylab=expression(log[10]~L / L['\u0298']))
+        abline(v=5777, lty=3, col='black')
+        abline(h=1, lty=3, col='black')
         magaxis(side=1:4, family=font, tcl=0.25, labels=c(1,1,0,0))
     } else {
-        lines(HR)
+        points(HR, pch=20, col=hrcol, cex=hrcex)
+        #lines(HR)
     }
-    points(HR, pch=1, lwd=0.1, 
-        col=brewer.pal(11,"Spectral")[floor(DF$age/13.9*11)+1],
-        cex=0.5*DF$radius/max(sims.DF$radius))
 }
+points(1, log10(5777), pch=1, cex=1)
+points(1, log10(5777), pch=20, cex=0.1)
 dev.off()
 
-cairo_pdf('JCD.pdf', width=4, height=3, family=font)
+
+png(file.path(plot_dir, 'JCD.png'), width=600, height=450, family=font)
 par(mar=c(3, 4, 1, 1), mgp=c(2, 0.25, 0), cex.lab=1.3)
-#for (simulation_i in 1:length(simulations)) {
-#    DF <- read.table(simulations[simulation_i], header=1)
-#    DF <- DF[DF$dnu02_median > 0 & DF$dnu02_median < 15,]
 for (simulation_i in 1:nrow(combos)) {
-    DF <- merge(combos[simulation_i,], sims.DF)
+    DF <- merge(combos[simulation_i,], seis.DF)
     JCD <- DF$dnu02_median ~ DF$Dnu_median
-    jcdcol <- brewer.pal(11,"Spectral")[floor(DF$age/13.9*11)+1]
-    jcdcex <- 0.5*DF$mass/max(sims.DF$mass)#max_M
+    jcdcol <- brewer.pal(10,"Spectral")[floor(DF$age/13.9*10)+1]
+    jcdcex <- 0.1*DF$M/max(seis.DF$M)#max_M
     if (simulation_i == 1) {
-        plot(JCD, pch=20, lwd=0.1, 
+        plot(JCD, pch=20, 
             col=jcdcol,
             cex=jcdcex, tcl=0,
-            #ylim=dnu_lim, xlim=Dnu_lim,
-            ylim=range(sims.DF$dnu02_median), xlim=range(sims.DF$Dnu_median),
+            ylim=range(seis.DF$dnu02_median), xlim=range(seis.DF$Dnu_median),
             xaxt='n', yaxt='n',
             xlab=expression(Delta*nu~"["*mu*Hz*"]"),
             ylab=expression(delta*nu[0*","*2]~"["*mu*Hz*"]"))
-        #abline(h=9, lty=3, col='lightgray')
-        #abline(v=136, lty=3, col='lightgray')
-        points(135, 9, pch=1, cex=1)
-        points(135, 9, pch=20, cex=0.1)
+        abline(v=135, lty=3, col='black')
+        abline(h=9, lty=3, col='black')
         magaxis(side=1:4, family=font, tcl=0.25, labels=c(1,1,0,0))
     } else {
         points(JCD, col=jcdcol, pch=20, cex=jcdcex)
     }
-    
 }
+points(135, 9, pch=1, cex=1)
+points(135, 9, pch=20, cex=0.1)
 dev.off()
 
-cairo_pdf('JCD-mesh.pdf', width=8, height=6, family=font)
+
+cairo_pdf(file.path(plot_dir, 'JCD-mesh.pdf'), width=8, height=6, family=font)
 par(mar=c(5, 6, 1, 0), mgp=c(2, 0.25, 0), cex.lab=1.3)
-#my.matrix  <- interp(Dnus,dnus,ages)
-my.matrix  <- interp(sims.DF$Dnu_median, sims.DF$dnu02_median, sims.DF$age)
-#ind.mat.na <- which(is.na(c(my.matrix$z)))
-#my.matrix$z[ind.mat.na] <- 0
+my.matrix  <- interp(seis.DF$Dnu_median, seis.DF$dnu02_median, seis.DF$age)
 my.heat.colors <- function(x) { rev(heat.colors(x, alpha=1)) }
 filled.contour(my.matrix,
-    #color=function(x) { brewer.pal(11,"Spectral")[x] },
     color=my.heat.colors,
     key.axes={
         axis(4, cex.axis=1, tcl=0)
         mtext("Age [Gyr]", side=4, las=3, line=3, cex=2)
     },
     plot.axes={
-        #abline(v=136, lty=3, col='black')
-        #abline(h=9, lty=3, col='black')
         contour(my.matrix, add=TRUE, nlevels=13, labcex=1)
         points(135, 9, pch=1, cex=1)
         points(135, 9, pch=20, cex=0.1)
-        magaxis(side=1:4, tcl=0.5, labels=c(1,1,0,0), cex.lab=0.5, family='Palatino')
+        magaxis(side=1:4, tcl=0.5, labels=c(1,1,0,0), cex.lab=0.5, 
+                family='Palatino')
     },
     plot.title={
         title(xlab=expression(Delta*nu~"["*mu*Hz*"]"), cex.lab=2, line=3)
-        title(ylab=expression(delta*nu[0*","*2]~"["*mu*Hz*"]"), cex.lab=2, line=3)
+        title(ylab=expression(delta*nu[0*","*2]~"["*mu*Hz*"]"), cex.lab=2, 
+              line=3)
     })
 dev.off()
 
 
-cairo_pdf('ratios-mesh.pdf', width=8, height=6, family=font)
+cairo_pdf(file.path(plot_dir, 'ratios-mesh.pdf'), 
+    width=8, height=6, family=font)
 par(mar=c(5, 6, 1, 0), mgp=c(2, 0.25, 0), cex.lab=1.3)
-#my.matrix  <- interp(r02,r01,ages)
-my.matrix  <- interp(sims.DF$r_sep02_median, sims.DF$r_avg01_median, sims.DF$age)
-#ind.mat.na <- which(is.na(c(my.matrix$z)))
-#my.matrix$z[ind.mat.na] <- 0
+my.matrix  <- interp(seis.DF$r_sep02_median, seis.DF$r_avg01_median, 
+    seis.DF$age)
 my.heat.colors <- function(x) { rev(heat.colors(x, alpha=1)) }
 filled.contour(my.matrix,
-    #color=function(x) { brewer.pal(11,"Spectral")[x] },
     color=my.heat.colors,
     key.axes={
         axis(4, cex.axis=1, tcl=0)
         mtext("Age [Gyr]", side=4, las=3, line=3, cex=2)
     },
     plot.axes={
-        #abline(v=136, lty=3, col='black')
-        #abline(h=9, lty=3, col='black')
         points(6.642727e-02, 2.238694e-02, pch=1, cex=1)
         points(6.642727e-02, 2.238694e-02, pch=20, cex=0.1)
         contour(my.matrix, add=TRUE, nlevels=13, labcex=1)
-        magaxis(side=1:4, tcl=0.5, labels=c(1,1,0,0), cex.lab=0.5, family='Palatino')
+        magaxis(side=1:4, tcl=0.5, labels=c(1,1,0,0), cex.lab=0.5, 
+                family='Palatino')
     },
     plot.title={
         title(xlab=expression(r[0*","*2]~"["*mu*Hz*"]"), cex.lab=2, line=3)
@@ -156,5 +169,41 @@ filled.contour(my.matrix,
 dev.off()
 
 
-#corrs <- do.call(cbind, Map(function (f) { a <- read.table(f, header=1); cor(a, a$age) } , simulations))
+cairo_pdf(file.path(plot_dir, 'rcd-mesh.pdf'), width=8, height=6, family=font)
+par(mar=c(5, 6, 1, 0), mgp=c(2, 0.25, 0), cex.lab=1.3)
+my.matrix  <- interp(seis.DF$r_sep02_median, seis.DF$dnu02_median, seis.DF$age)
+my.heat.colors <- function(x) { rev(heat.colors(x, alpha=1)) }
+filled.contour(my.matrix,
+    color=my.heat.colors,
+    key.axes={
+        axis(4, cex.axis=1, tcl=0)
+        mtext("Age [Gyr]", side=4, las=3, line=3, cex=2)
+    },
+    plot.axes={
+        points(0.06642727, 9, pch=1, cex=1)
+        points(0.06642727, 9, pch=20, cex=0.1)
+        contour(my.matrix, add=TRUE, nlevels=13, labcex=1)
+        magaxis(side=1:4, tcl=0.5, labels=c(1,1,0,0), cex.lab=0.5, 
+                family='Palatino')
+    },
+    plot.title={
+        title(xlab=expression(r[0*","*2]~"["*mu*Hz*"]"), cex.lab=2, line=3)
+        title(ylab=expression(dnu[0*","*2]~"["*mu*Hz*"]"), cex.lab=2, line=3)
+    })
+dev.off()
 
+
+cairo_pdf(file.path(plot_dir, 'r02-age.pdf'), width=4, height=3, family=font)
+par(mar=c(3, 4, 1, 1), mgp=c(2, 0.25, 0), cex.lab=1.3)
+plot(seis.DF$age ~ seis.DF$r_sep02_median, pch=3, tcl=0, cex=0.1,
+     xaxt='n', yaxt='n',
+     xlab=expression(r[0*","*2]~"["*mu*Hz*"]"), ylab='Age [Gyr]')
+magaxis(side=1:4, tcl=0.5, labels=c(1,1,0,0), family='Palatino')
+dev.off()
+
+
+predict(lm(age ~ poly(r_sep02_median,6)*poly(r_avg01_median,6), data=seis.DF), 
+        data.frame(r_sep02_median=6.642727e-02, r_avg01_median=2.238694e-02), 
+        interval="predict", level=0.68)
+#corrs <- do.call(cbind, Map(function (f) { 
+#    a <- read.table(f, header=1); cor(a, a$age) } , simulations))
