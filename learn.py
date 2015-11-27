20703094b6fa4c5468348c9d95dff98b879258d7
Earl Bellinger <earlbellinger@gmail.com>
2015-11-27 13:13:27 +0100

    Update learn.py

--- learn.py
+++ learn.py
@@ -26,8 +26,8 @@ mpl.rcParams.update({'font.size': 16})
 import pylab as P
 
 y_latex = ("$M_0$", "$Y_0$", "$Z_0$", "$\\alpha_{\mathrm{MLT}}$", 
-    "\mathrm{Age}")
-y_names = ["M", "Y", "Z", "alpha", "age"]
+    "\mathrm{Age}", "R", "He", "$\\log g$", "L")
+y_names = ["M", "Y", "Z", "alpha", "age", "radius", "He", "log_g", "L"]
 
 """
 X_names = ["$\mathrm{radius}$", "$L$", "$T_{\mathrm{eff}}$", "$\log g$", 
@@ -39,21 +39,14 @@ X_names = ["$\mathrm{radius}$", "$L$", "$T_{\mathrm{eff}}$", "$\log g$",
 """
 
 ### Load data
-#data = np.loadtxt('grids/sobol_grid-diffusion.dat', skiprows=1)
 data = pd.read_csv('grids/deleter.dat', sep='\t')
-exclude_pattern = "He|log_g|mass|nu_max|Dnu"
+exclude_pattern = "mass|nu_max|Dnu|dnu13|r_sep13|radial_velocity"
 data = data.drop([i for i in data.columns 
     if re.search(exclude_pattern, i)], axis=1)
 X = data.drop(y_names, axis=1)
 ys = data.drop(X.columns, axis=1)
-#ys['age'] = np.log10(ys['age']/10**9)# / 10**9
 print(X.head())
-#X = np.array(data)[:,5:]
-#del_indices = [X_names.index("Dnu_3"), X_names.index("eps_3")]
-#X = np.delete(X, del_indices, 1)
-#X_names = np.delete(X_names, del_indices)
-#ys = np.array(data)[:,:5]
-#ys[:,-2] = ys[:,-2]/10**9
+exclude_pattern = exclude_pattern + "|" + "|".join(y_names)
 
 ### Train model
 """
@@ -83,7 +76,7 @@ forest = Pipeline(steps=[
     ('rf', RandomForestRegressor(n_estimators=100, n_jobs=1, verbose=1,
             oob_score=1, bootstrap=1))])
 """
-forest = ExtraTreesRegressor(n_estimators=2000, n_jobs=-1, verbose=1,
+forest = RandomForestRegressor(n_estimators=1000, n_jobs=1, verbose=1,
     oob_score=1, bootstrap=1)
 start = time()
 forest.fit(X, ys)#new_ys)
@@ -94,17 +87,20 @@ print(est.oob_score_)
 #joblib.dump(forest, 'rf.pickle')
 
 ### Run on all stars in the perturb directory
-print("Star\t\tM\t\tY\t\tZ\t\talpha\t\tage")
-for star_fname in os.listdir('perturb'):
+print("Star\t\tM\t\tY\t\tZ\t\talpha\t\tage\t\tradius\t\tL\t\tHe\t\tlog g")
+fnames = [os.path.join(dp, f) 
+          for dp, dn, fn in os.walk(os.path.expanduser("perturb")) 
+          for f in fn]
+for star_fname in fnames:#os.listdir('perturb'):
+    if (".dat" not in star_fname): continue
     #star_data = np.loadtxt(os.path.join('perturb', star_fname), skiprows=1)
     #star_data = np.delete(star_data, del_indices, 1)
-    star_data = pd.read_csv(os.path.join('perturb', star_fname), sep='\t')
+    star_data = pd.read_csv(star_fname, sep='\t')
     star_data = star_data.drop([i for i in star_data.columns 
         if re.search(exclude_pattern, i)], axis=1)
+    star_data = star_data.dropna()
     predict = forest.predict(star_data)
     #predict = y_trfm.inverse_transform(predict)
-    #predict[:,y_names.index('age')] /= 10**9
-    #predict[:,y_names.index('age')] = 10**predict[:,y_names.index('age')]
     middles = np.median(predict, 0)
     stds = np.std(predict, 0)
     outstr = star_fname.split("_")[0]
@@ -113,21 +109,26 @@ for star_fname in os.listdir('perturb'):
     for (col_j, name) in enumerate(y_names):
         (m, s) = (middles[col_j], stds[col_j])
         outstr += "\t%.3g +/- %.3g" % (m, s)
-        if (col_j == 4):
-            ax = plt.subplot2grid((3,4), (2,1), colspan=2)
-        else:
-            ax = plt.subplot2grid((3,4), (int(col_j/2),(col_j%2)*2), colspan=2)
-        #ax = plt.subplot(3,2,col_j+1)
+        #if (col_j == 4):
+        #    ax = plt.subplot2grid((3,4), (2,1), colspan=2)
+        #else:
+        #    ax = plt.subplot2grid((3,4), (int(col_j/2),(col_j%2)*2), colspan=2)
+        ax = plt.subplot(3,3,col_j+1)
         
         n, bins, patches = P.hist(predict[:,col_j], 50, normed=1, 
             histtype='stepfilled', color='white')
-        #y = P.normpdf(bins, m, s)
-        #P.plot(bins, y, 'b--', linewidth=1.5)
+        y = P.normpdf(bins, m, s)
+        P.plot(bins, y, 'b--', linewidth=1.5)
+        
+        ax.annotate(r"$\epsilon = %.3g\%%$" % (s/m*100),
+            xy=(0.5, 0.125), xycoords='axes fraction', #fontsize=16,
+            horizontalalignment='center', verticalalignment='center')
         
         P.xlabel(y_latex[col_j])
         ax.set_yticklabels('',visible=False)
-        P.locator_params(axis='x', nbins=5)
-        xticks = [m-4*s, m-2*s, m, m+2*s, m+4*s]
+        P.locator_params(axis='x', nbins=3)
+        #xticks = [m-4*s, m-2*s, m, m+2*s, m+4*s]
+        xticks = [m-3*s, m, m+3*s]
         ax.set_xlim([m-4*s, m+4*s])
         ax.set_xticks(xticks)
         ax.set_xticklabels(['%.3g'%xtick for xtick in xticks])
@@ -136,6 +137,7 @@ for star_fname in os.listdir('perturb'):
     
     plt.subplots_adjust(top=0.9)
     plt.savefig(os.path.join('learn_plots', star_fname.split("_")[0] + '.png'))
+    plt.close()
     """
     plt.figure(figsize=(8, 6), dpi=80, facecolor='w', edgecolor='k')
     plt.matshow(pd.DataFrame(predict, columns=y_names).corr())
@@ -145,10 +147,6 @@ for star_fname in os.listdir('perturb'):
     print(outstr)
 
 ### Test on 16 Cyg A & B
-#cygA = np.loadtxt('perturb/16CygA_perturb.dat', skiprows=1)
-#cygB = np.loadtxt('perturb/16CygB_perturb.dat', skiprows=1)
-#cygA = np.delete(cygA, del_indices, 1)
-#cygB = np.delete(cygB, del_indices, 1)
 cygA = pd.read_csv('perturb/16CygA_perturb.dat', sep='\t')
 cygA = cygA.drop([i for i in cygA.columns 
     if re.search(exclude_pattern, i)], axis=1)
@@ -160,42 +158,41 @@ start = time()
 #pred_B = y_trfm.inverse_transform(forest.predict(cygB))
 pred_A = forest.predict(cygA)
 pred_B = forest.predict(cygB)
-"""
-pred_A = y_trfm.inverse_transform(pred_A)
-pred_A[:,y_names.index('age')] /= 10**9
-pred_B = y_trfm.inverse_transform(pred_B)
-pred_B[:,y_names.index('age')] /= 10**9
-"""
 end = time()
 print("%.3g seconds to predict Cyg A and B" % (end-start))
 plt.figure(figsize=(8, 6), dpi=80, facecolor='w', edgecolor='k')
 plt.suptitle("16 Cyg")
 for (col_j, name) in enumerate(y_names):
-    if (col_j == 4):
-        ax = plt.subplot2grid((3,4), (2,1), colspan=2)
-    else:
-        ax = plt.subplot2grid((3,4), (int(col_j/2),(col_j%2)*2), colspan=2)
-    #ax = plt.subplot(3,2,col_j+1)
+    #if (col_j == 4):
+    #    ax = plt.subplot2grid((3,4), (2,1), colspan=2)
+    #else:
+    #    ax = plt.subplot2grid((3,4), (int(col_j/2),(col_j%2)*2), colspan=2)
+    ax = plt.subplot(3,3,col_j+1)
     
     (m_A, s_A) = (np.median(pred_A[:,col_j]), np.std(pred_A[:,col_j]))
     (m_B, s_B) = (np.median(pred_B[:,col_j]), np.std(pred_B[:,col_j]))
     
     n, bins, patches = P.hist(pred_A[:,col_j], 50, normed=1, 
         histtype='stepfilled', color='red', alpha=0.5)
-    #y = P.normpdf(bins, m_A, s_A)
-    #P.plot(bins, y, 'r--', linewidth=1.5)
+    y = P.normpdf(bins, m_A, s_A)
+    P.plot(bins, y, 'r--', linewidth=1.5)
     
     n, bins, patches = P.hist(pred_B[:,col_j], 50, normed=1, 
         histtype='stepfilled', color='blue', alpha=0.5)
-    #y = P.normpdf(bins, m_B, s_B)
-    #P.plot(bins, y, 'b--', linewidth=1.5)
+    y = P.normpdf(bins, m_B, s_B)
+    P.plot(bins, y, 'b--', linewidth=1.5)
     
     P.xlabel(y_latex[col_j])
     ax.set_yticklabels('',visible=False)
-    P.locator_params(axis='x', nbins=5)
-    xticks = np.linspace(min(m_A-4*s_A, m_B-4*s_B), 
-                         max(m_A+4*s_A, m_B+4*s_B), 5)
-    ax.set_xlim([min(xticks), max(xticks)])
+    #P.locator_params(axis='x', nbins=5)
+    #xticks = np.linspace(min(m_A-4*s_A, m_B-4*s_B), 
+    #                     max(m_A+4*s_A, m_B+4*s_B), 5)
+    P.locator_params(axis='x', nbins=3)
+    xticks = np.linspace(min(m_A-3*s_A, m_B-3*s_B), 
+                         max(m_A+3*s_A, m_B+3*s_B), 3)
+    #ax.set_xlim([min(xticks), max(xticks)])
+    ax.set_xlim([min(m_A-4*s_A, m_B-4*s_B), 
+                 max(m_A+4*s_A, m_B+4*s_B)])
     ax.set_xticks(xticks)
     ax.set_xticklabels(['%.3g'%xtick for xtick in xticks])
     ax.minorticks_on()
@@ -203,23 +200,7 @@ for (col_j, name) in enumerate(y_names):
 
 plt.subplots_adjust(top=0.9)
 plt.savefig(os.path.join('learn_plots', '16Cyg.png'))
-
-"""
-print("\t16CygA\t16CygB")
-for (col_j, name) in enumerate(y_names):
-    cyg_A_mean = np.mean(pred_A[:,col_j])
-    cyg_A_std = np.std(pred_A[:,col_j])
-    cyg_B_mean = np.mean(pred_B[:,col_j])
-    cyg_B_std = np.std(pred_B[:,col_j])
-    if name=="age":
-        cyg_A_mean = cyg_A_mean / 10**9
-        cyg_A_std  = cyg_A_std  / 10**9
-        cyg_B_mean = cyg_B_mean / 10**9
-        cyg_B_std  = cyg_B_std  / 10**9
-    print("%s\t%.3g ± %.3g\t%.3g ± %.3g" % (name, 
-            cyg_A_mean, cyg_A_std, 
-            cyg_B_mean, cyg_B_std))
-"""
+plt.close()
 
 ### Plot importances 
 #est = forest.steps[1][1]
